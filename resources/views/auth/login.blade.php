ca1f30e375fddbd7671ec7f5966f8244cd675e57
Afrees <contactoomunoz@gmail.com>
2025-05-13 10:38:28 -0500

    Commit Gabriel detalles y demas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div id="background" class="fixed inset-0 bg-cover bg-center flex items-center justify-center opacity-0 transition-opacity duration-700"
      style="background-image: url('https://images.unsplash.com/photo-1534146789009-76ed5060ec70?q=80&w=1909&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');">
-     <div id="login-form" class="animate__animated animate__fadeInDown bg-white bg-opacity-90 backdrop-blur-md shadow-2xl rounded-xl p-8 w-full max-w-md">
+     <div id="login-form" class="animate__animated animate__fadeInDown bg-black/5 bg-opacity-10  backdrop-blur-md shadow-2xl rounded-xl p-8 w-full max-w-md border-2  border-amber-100">
 
-        <div class="text-center text-blue-700 font-bold text-2xl mb-6">
+        <div class="text-center text-white  font-bold text-2xl mb-6">
             {{ __('Iniciar Sesión') }}
         </div>
 
@@ -14,9 +14,9 @@
 
             {{-- Email --}}
             <div class="mb-4">
-                <label for="email" class="block text-sm font-semibold text-blue-900 mb-1">{{ __('Correo Electrónico') }}</label>
+                <label for="email" class="block text-sm font-semibold text-white  mb-1">{{ __('Correo Electrónico') }}</label>
                 <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus
-                    class="w-full px-4 py-2 border border-blue-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 @error('email') border-red-500 @enderror">
+                    class="w-full px-4 py-2 border border-blue-100 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-600 @error('email') border-red-500 @enderror">
                 @error('email')
                     <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
                 @enderror
@@ -24,9 +24,9 @@
 
             {{-- Password --}}
             <div class="mb-4">
-                <label for="password" class="block text-sm font-semibold text-blue-900 mb-1">{{ __('Contraseña') }}</label>
+                <label for="password" class="block text-sm font-semibold text-white  mb-1">{{ __('Contraseña') }}</label>
                 <input id="password" type="password" name="password" required
-                    class="w-full px-4 py-2 border border-blue-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 @error('password') border-red-500 @enderror">
+                    class="w-full px-4 py-2 border border-blue-100 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-600 @error('password') border-red-500 @enderror">
                 @error('password')
                     <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
                 @enderror
@@ -34,30 +34,30 @@
 
             {{-- Recordarme --}}
             <div class="mb-4 flex items-center">
-                <input type="checkbox" name="remember" id="remember" class="mr-2 text-blue-600" {{ old('remember') ? 'checked' : '' }}>
-                <label for="remember" class="text-sm text-blue-900">{{ __('Recordarme') }}</label>
+                <input type="checkbox" name="remember" id="remember" class="mr-2 text-blue-300 bg " {{ old('remember') ? 'checked' : '' }}>
+                <label for="remember" class="text-sm text-white ">{{ __('Recordarme') }}</label>
             </div>
 
             {{-- Botones --}}
             <div class="flex items-center justify-center space-x-1  ">
                 <button type="submit"
-                    class="bg-blue-600 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 transition">
+                    class="bg-cyan-900 hover:bg-slate-600  text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 transition">
                     {{ __('Entrar') }}
                 </button>
                 <a href="{{ route('register') }}"
-                    class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 m-6 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 transition">
+                    class="bg-cyan-900 hover:bg-slate-600 text-white font-bold py-2 px-4 m-6 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 transition">
                     {{ __('Registrar') }}
                 </a>
             </div>
 
             {{-- Iniciar con --}}
             <div class="w-full text-center mt-6">
-                <label class="block text-sm text-blue-900 mb-2">{{ __('Iniciar sesión con') }}</label>
+                <label class="block text-sm text-white mb-2">{{ __('Iniciar sesión con') }}</label>
                 <div class="flex justify-center space-x-6">
-                    <a href="#" class="text-blue-600 text-2xl hover:scale-125 transition-transform duration-300">
+                    <a href="#" class="text-white text-2xl hover:scale-125 transition-transform duration-300">
                         <i class="bi bi-google"></i>
                     </a>
-                    <a href="#" class="text-gray-800 text-2xl hover:scale-125 transition-transform duration-300">
+                    <a href="#" class="text-gray-100 text-2xl hover:scale-125 transition-transform duration-300">
                         <i class="bi bi-github"></i>
                     </a>
                 </div>
@@ -66,7 +66,7 @@
             {{-- Recuperar contraseña alineado a la izquierda --}}
             <div class="mt-6 text-left">
                 @if (Route::has('password.request'))
-                    <a href="{{ route('password.request') }}" class="text-sm text-blue-600 hover:underline">
+                    <a href="{{ route('password.request') }}" class="text-sm text-white  hover:underline">
                         {{ __('¿Olvidaste tu contraseña?') }}
                     </a>
                 @endif
--- resources/views/includes/features.blade.php
+++ resources/views/includes/features.blade.php
@@ -8,34 +8,34 @@
             @php
                 $tools = [
                     [
-                        'title' => 'Montañas Majestuosas',
-                        'description' => 'Explorá cumbres nevadas y aire puro en cada rincón.',
-                        'image' => asset('images/prueba.png'),
+                        'title' => 'Comunidad',
+                        'description' => 'Divierte con nuevas personas y entrate en nuevas aventuras.',
+                        'image' => ('https://images.unsplash.com/photo-1684284994249-935f478cc127?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
                     ],
                     [
-                        'title' => 'Ciudades Vibrantes',
-                        'description' => 'Sumergite en el dinamismo y cultura de grandes urbes.',
-                        'image' => asset('images/prueba.png'),
+                        'title' => 'Mapa',
+                        'description' => 'Todo lo que necesitas, simplemente abriendo el mapa .',
+                        'image' => ('https://images.unsplash.com/photo-1633230329619-70ae2e6d50bb?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
                     ],
                     [
-                        'title' => 'Playas Paradisíacas',
-                        'description' => 'Relax garantizado en arenas blancas y aguas cristalinas.',
-                        'image' => asset('images/prueba.png'),
+                        'title' => 'Noticias',
+                        'description' => 'Aburrido?.. No te pierdas nuestras noticias del dia.',
+                        'image' => ('https://images.unsplash.com/photo-1650447973233-89e3971e1568?q=80&w=1827&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
                     ],
                     [
-                        'title' => 'Bosques Encantados',
-                        'description' => 'Naturaleza densa y misteriosa para reconectar.',
-                        'image' => asset('images/prueba.png'),
+                        'title' => 'Estadisticas',
+                        'description' => 'Accede sencillamente a tu apartado de estadisticas personalizables.',
+                        'image' => ('https://images.unsplash.com/photo-1625296276703-3fbc924f07b5?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
                     ],
                     [
-                        'title' => 'Desiertos Místicos',
-                        'description' => 'Explorá paisajes áridos llenos de historia y calor.',
-                        'image' => asset('images/prueba.png'),
+                        'title' => 'Puntos de Interes',
+                        'description' => 'Explorá rutas llenas de paisajes extrabagantes y nuevas aventuras.',
+                        'image' => ('https://images.unsplash.com/photo-1534787238916-9ba6764efd4f?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
                     ],
                     [
-                        'title' => 'Aventuras en el Ártico',
-                        'description' => 'Desafía el hielo y descubrí la belleza polar.',
-                        'image' => asset('images/prueba.png'),
+                        'title' => 'Retos',
+                        'description' => 'Desafiate a ti mismo a empezar y compite con las demas personas.',
+                        'image' => ('https://images.unsplash.com/photo-1672768612410-cf6eb6da24b0?q=80&w=1853&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
                     ],
                 ];
             @endphp
